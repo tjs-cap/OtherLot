5f5f88242cb9b20b5f2b092cb39167bebdb50088
Trevor Smith <tjs_uk@outlook.com>
2022-06-09 12:21:33 +0100

    Initial commit

---
+++ src/MyProgram.java
@@ -0,0 +1,18 @@
+public class MyProgram {
+
+
+
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 6;
+        double num3 = 3.1;
+        double num4 = 5.6;
+
+
+    }
+    public static void sumNumbers(int a, int b){
+        int sum = a + b;
+        System.out.println("Sum of " + a + "," + b + " is " + sum);
+    }
+
+}
